82f47c6e1f041c608ef7c9093a28a127d8749270
KARAN RANA <103360966+karantec@users.noreply.github.com>
2025-12-25 22:48:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        // <Route path="/login" element={<Home />} />
+    {/* <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<Login />} />
         <Route
           path="/dashboard"
